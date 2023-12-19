c8c310b34685e3370c3904d3320a7d9ba3401af4
Sauel Almonte <almontesauel@gmail.com>
2023-12-19 03:05:47 -0500

    Final Project Pizza Shop

--- script/script.js
+++ script/script.js
@@ -1,11 +1,9 @@
 const orderBtn = document.querySelector(".order-btn");
 
-
 const redirectToPage = () => {
 	const url = "../pages/order.html";
 	window.location.href = url;
 };
-
 
 document.addEventListener("DOMContentLoaded", () => {
 	orderBtn.addEventListener("click", redirectToPage);
